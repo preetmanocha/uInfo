48ff4683d3eefabacdd8e0e5e2d02229dd00a703
preetsingh <manocha.preet@gmail.com>
2019-03-18 23:57:47 -0700

    second commit

--- server.js
+++ server.js
@@ -17,18 +17,17 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
 let city = req.body.city;
-let url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=6dde3692bc68b2c685b0f6ceefa280ff'
+let url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imperial&appid=6dde3692bc68b2c685b0f6ceefa280ff'
     var ipp = (req.headers['x-forwarded-for'] || '').split(',').pop() ||
         req.connection.remoteAddress ||
         req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress);
+        req.connection.socket.remoteAddress;
         
     console.log("city");
     request(url, function (err, response, body) {
         if (err) {
             res.render('index', {weather: null,error: 'Error, please try again'});
             } else {
-                process.stdout.write("hello: ");
                 let weather = JSON.parse(body);
                 console.log(weather);
                 if (weather.main == undefined) {
